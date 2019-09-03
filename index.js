73c5105dfd624c0a5077c0f3e47c2d74c4359f00
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-03 01:28:32 +0100

    WIP: initial commit, setup get requests for get users & get users by Id

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// implement your API here
+const express = require('express')
+
+const db = require('./data/db')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to get users'
+      })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params
+  db.findById(id)
+    .then(user => {
+      user ? res.json(user) : res.status(404).json({
+        message: 'invaled user id'
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'failed to get user by id'
+      })
+    })
+})
+
+server.listen(3636, () => {
+  console.log('Server is running on port 3636...')
+})
