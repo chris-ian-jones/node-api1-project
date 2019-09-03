edf6ea125f7e3b6c13f7952393ea0a5e2df9f6cd
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-03 02:20:31 +0100

    WIP: setup PUT endpoint

--- index.js
+++ index.js
@@ -75,6 +75,36 @@ server.delete('/api/users/:id', (req, res) => {
     })
 })
 
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params
+  const changes = req.body
+
+  db.update(id, changes)
+    .then(user => {
+      if (user) {
+        if ( req.body.name === undefined || req.body.bio === undefined) {
+          res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+          }) 
+        } else {
+          res.status(200).json({
+            changes
+          })
+        }
+      } else {
+        res.status(404).json({
+          message: 'The user with the specified ID does not exist.'
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The user information could not be modified."
+      })
+    })
+})
+
+
 server.listen(3636, () => {
   console.log('Server is running on port 3636...')
 })
